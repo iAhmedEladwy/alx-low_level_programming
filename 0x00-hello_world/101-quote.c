ac0939b1f50ba3b02898d027fea82ce5e36072e0
Ahmed Eladwy <eladwy.ahmed@gmail.com>
2023-02-27 10:53:11 -0800

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#define STDERR_FILENO 2
+/**
+ * main - Entry point
+ * Return:Always 1 (Success)
+ */
+int main(void)
+{
+char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+write(STDERR_FILENO, message, sizeof(message\n) - 1);
+return (1);
+}
