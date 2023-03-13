bd352aed06b844b21ab920222df3a43f1706344b
Ahmed Eladwy <eladwy.ahmed@gmail.com>
2023-03-13 10:19:50 -0700

    fix alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * main - prints a-z, followed by a new line
+ * main - print_alphabet() prints a-z, followed by a new line
 */
 void print_alphabet(void)
 {
 char x;
-for (x = 'a'; x <= 'z'x++)
+for (x = 'a'; x <= 'z'; x++)
 	_putchar(x);
 _putchar('\n');
 }
